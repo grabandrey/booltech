ee88c78b42a4eb31c6a34d7dd4110c452b6d6564
Andrei Grab <grabandrey93@gmail.com>
2025-10-15 22:31:16 +0300

    adding cool wallpaper

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,11 @@
+function Navigation() {
+    return (
+        <div className="fixed top-0 left-0 w-full z-10 p-5">
+            <div className="border-[1px] border-[rgba(255,255,255,0.1)] w-full h-20 rounded-full text-white bg-[rgba(0,0,0,0.3)] backdrop-blur-[50px] justify-center items-center grid">
+                <div>Navigation</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter, Route, Routes} from "react-router";
+import LiquidEther from "./Components/LiquidEther.jsx";
+import Navigation from "./Components/Navigation.jsx";
 
 createRoot(document.getElementById('root')).render(
       <BrowserRouter>
+          <div className="absolute top-0 left-0 w-full h-full z-0 opacity-40">
+              <LiquidEther
+                  colors={[ '#2eff27', '#baff9f', '#b2ef9e' ]}
+                  mouseForce={50}
+                  cursorSize={100}
+                  isViscous={true}
+                  viscous={30}
+                  iterationsViscous={32}
+                  iterationsPoisson={32}
+                  resolution={0.5}
+                  isBounce={false}
+                  autoDemo={true}
+                  autoSpeed={0.5}
+                  autoIntensity={2.2}
+                  takeoverDuration={0.25}
+                  autoResumeDelay={1000}
+                  autoRampDuration={0.6}
+              />
+          </div>
+          <Navigation />
           <Routes>
               <Route index element={<App />} />
           </Routes> 
